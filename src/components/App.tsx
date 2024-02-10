ca3fd78acc145447341136418225451ac7625862
john-mirage <28307090+john-mirage@users.noreply.github.com>
2024-02-10 20:47:35 +0100

    fix: fix accessibility

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,10 +2,10 @@ import Card from "./Card"
 
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen px-24 py-48 bg-black">
+    <main className="flex flex-col justify-center items-center min-h-screen px-24 py-48 bg-black">
       <h1 className="sr-only">Social links profile</h1>
       <Card />
-    </div>
+    </main>
   )
 }
 
